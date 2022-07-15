ee1702db3d04627f22714fd7a47c0f9a9ce77f0d
Pedro Pinheiro <pedro.hsuzanopinheiro@gmail.com>
2022-07-15 15:56:44 -0300

    creating cloud composer resource

--- main.tf
+++ main.tf
@@ -15,24 +15,35 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+/*
+resource "google_compute_network" "test" {
+  name                    = "composer-test-network3"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
+resource "google_compute_subnetwork" "test" {
+  name          = "composer-test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  network       = google_compute_network.test.id
+}
 
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
+resource "google_service_account" "test" {
+  account_id   = "composer-env-account"
+  display_name = "Test Service Account for Composer Environment"
+}
 
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
+resource "google_project_iam_member" "composer-worker" {
+  role    = "roles/composer.ServiceAgentV2Ext"
+  member  = "serviceAccount:${google_service_account.test.email}"
+}
+*/
+
+resource "google_composer_environment" "test" {
+  name   = "example-composer-env-tf-c2"
+  
+ config {
+    software_config {
+      image_version = "composer-2.0.1-airflow-2.1.4"
     }
   }
 }
